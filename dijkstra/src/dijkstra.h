f9f9fa804e99508ffa02ab36c84260c5bb524484
Yuki Tsuboi <y.t.1985.11.7@gmail.com>
2024-06-21 09:27:02 +0900

    [update] dijkstra/ updated

--- dijkstra/src/dijkstra.h
+++ dijkstra/src/dijkstra.h
@@ -8,6 +8,8 @@
 #define DIJKSTRA_SRC_DIJKSTRA_H_
 
 #include <iostream>
+#include <limits>
+#include <queue>
 #include <sstream>
 #include <tuple>
 #include <vector>
@@ -16,6 +18,7 @@ constexpr int MIN_N = 1;
 constexpr int MAX_N = 5000;
 constexpr int MIN_R = 1;
 constexpr int MAX_R = 100000;
+constexpr int INF = std::numeric_limits<int>::max();
 
 /**
  * @brief Validates the input constraints for N and R.
@@ -25,6 +28,14 @@ constexpr int MAX_R = 100000;
  */
 bool InputValidation(int N, int R);
 
+/**
+ * @brief Finds the length of the second shortest path from the first intersection to the N-th intersection.
+ * @param N Number of intersections.
+ * @param R Number of bidirectional roads.
+ * @param roads A vector of tuples representing the roads. Each tuple contains three integers: u, v, and w.
+ *              u and v are the endpoints of a road, and w is the weight (length) of the road.
+ * @return The length of the second shortest path from intersection 1 to intersection N. If no such path exists, returns -1.
+ */
 int dijkstra(int N, int R,
              const std::vector<std::tuple<int, int, int>>& roads);
 
