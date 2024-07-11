d107451c3df214f211eac5f0b93a94ee2137e461
Yuki Tsuboi <y.t.1985.11.7@gmail.com>
2024-07-11 11:19:27 +0900

    [update] sieve-of-eratosthenes/ unit test implemented

--- number_theory/sieve-of-eratosthenes/test/test_sieve-of-eratosthenes.cpp
+++ number_theory/sieve-of-eratosthenes/test/test_sieve-of-eratosthenes.cpp
@@ -7,6 +7,51 @@
 #include <gtest/gtest.h>
 #include "src/sieve-of-eratosthenes.h"
 
+class SieveOfEratosthenesTest : public ::testing::Test {
+ protected:
+  int n;
+
+  void SetUp() override {
+    // Initialize default values for tests
+    n = 1000000;
+  }
+
+  void TearDown() override {
+    // Clean up common resources for tests
+  }
+};
+
+TEST_F(SieveOfEratosthenesTest, BasicPrimeCount) {
+  // Test basic prime count up to n
+  EXPECT_TRUE(InputValidation(n));
+  EXPECT_EQ(SieveOfEratosthenes(n), 78498);
+}
+
+TEST_F(SieveOfEratosthenesTest, MinValue) {
+  // Test the validation of input where n is at its minimum value
+  n = 1;
+  EXPECT_TRUE(InputValidation(n));
+  EXPECT_EQ(SieveOfEratosthenes(n), 0);
+}
+
+TEST_F(SieveOfEratosthenesTest, MaxValue) {
+  // Test the validation of input where n exceeds the maximum allowed
+  n = kMaxN;
+  EXPECT_TRUE(InputValidation(n));
+}
+
+TEST_F(SieveOfEratosthenesTest, OverMaxValue) {
+  // Test the validation of input where n exceeds the maximum allowed
+  n = kMaxN + 1;
+  EXPECT_FALSE(InputValidation(n));
+}
+
+TEST_F(SieveOfEratosthenesTest, InvalidInput) {
+  // Test invalid input
+  n = -1;
+  EXPECT_FALSE(InputValidation(n));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
